7c24cbaa0d047c3837434543e9f983c594b23ae3
PC-Escola <hugo.allnet@gmail.com>
2018-04-19 19:26:21 -0300

    Provas concluídas para apresentar respostas aleatórias em cada questão para os alunos

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/professorHugo/experienceV1.git
 git push -u origin master
+
+19/04/18 19h24m - "Provas concluídas para apresentar respostas aleatórias em cada questão para os alunos. Próxima etapa, fazer a resposta ser enviada para o banco de dados e fazer update na table de usuarios com a nota da prova do aluno, e impedir do mesmo fazer a prova novamente"
--- app/pages/parts/perguntasAluno.php
+++ app/pages/parts/perguntasAluno.php
@@ -13,30 +13,22 @@ while($ResBuscarQuestoes = mysql_fetch_assoc($ExeQrBuscarQuestoesProvas)){
   <div class="form-group col-md-12">
     <h3>Questão<?php echo $ResBuscarQuestoes['id_prova'].":</b><br> " ;echo $ResBuscarQuestoes['pergunta_prova']?></h3>
     <h4><?php echo $ResBuscarQuestoes['modulo_prova']?></h4>
-    <label class="col-md-3 text-justify" style="font-weight: 100;border:1px solid #ddd;min-height:100px">
-      <input type="radio" name="<?php echo $ResBuscarQuestoes['modulo_prova']?>" value="1">
+
         <?php
         $ResBuscarQuestoes['id_resposta_correta'];
-        $QueryBuscarRespostaCertaProva = "SELECT * FROM respostas_corretas WHERE id = $ResBuscarQuestoes[id_prova]";
+        $QueryBuscarRespostaCertaProva = "SELECT * FROM respostas_provas WHERE id_pergunta = $ResBuscarQuestoes[id_prova] ORDER BY RAND() LIMIT 4";
         $ExeQrBuscarRespostaCertaProva = mysql_query($QueryBuscarRespostaCertaProva);
         while($Resposta = mysql_fetch_assoc($ExeQrBuscarRespostaCertaProva)){
-          echo $Resposta['resposta'];
+          // print_r($Resposta);
+          ?>
+          <p></p>
+          <label class="col-md-3 text-justify" style="font-weight: 100;border:1px solid #ddd;min-height:100px">
+            <input type="radio" name="<?php echo $Resposta['id_pergunta']?>" value="<?php echo $Resposta['verdadeira']?>">
+            <?php echo $Resposta['resposta'];?>
+          </label>
+          <?php
         }
       ?>
-    </label>
-
-    <?php
-      $QueryBuscarRespostasErradas = "SELECT * FROM respostas_erradas WHERE id_pergunta = $ResBuscarQuestoes[id_prova]";
-      $ExeQrBuscarRespostasErradas = mysql_query($QueryBuscarRespostasErradas);
-      while($RespostasErradas = mysql_fetch_assoc($ExeQrBuscarRespostasErradas)){
-        ?>
-        <label class="col-md-3 text-justify" style="font-weight:100;border:1px solid #ddd;min-height:100px">
-          <input type="radio" name="<?php echo $ResBuscarQuestoes['modulo_prova']?>" value="1">
-          <?php echo $RespostasErradas['resposta_errada']; ?>
-        </label>
-        <?php
-      }
-    ?>
   </div>
   <hr style="border-color:#333;">
   <?php
