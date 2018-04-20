306d61108aee5d348b9433d937d3e02eedf2bced
professorHugo <hugo.allnet@gmail.com>
2018-04-20 17:40:09 -0300

    Permissão de usuarios com updade do banco de dados

--- app/index.php
+++ app/index.php
@@ -8,6 +8,21 @@ $Matricula = $_SESSION['Login']['matricula'];
 $Senha = $_SESSION['Login']['senha'];
 $PermLogado = $_SESSION['Login']['permissao'];
 
+$QueryAtualizarSession = "SELECT * FROM usuarios WHERE matricula = '$Matricula'";
+$ExeQrAtualizarSession = mysql_query($QueryAtualizarSession);
+"<br>";
+while($ResUpdateSession = mysql_fetch_assoc($ExeQrAtualizarSession)){
+  "Session up to date<br>";
+  $_SESSION['Login']['permissao'] = $ResUpdateSession['permissao'];
+  if($_SESSION['Login']['permissao'] == 0){
+    "Permissão de Aluno";
+  }else if($_SESSION['Login']['permissao'] == 1){
+    "Permissão de professor";
+  }else{
+    "Permissão de Administrador";
+  }
+}
+
 if ($PermLogado == 2) {
     $PermLogado = "Administrador";
 } else {
